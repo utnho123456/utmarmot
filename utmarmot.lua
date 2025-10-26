b5f19155a3db6e84ee09cff4a34b790b42fa4e35
utnho123456 <baolonggl28022021@gmail.com>
2025-10-26 19:07:00 +0700

    Update utmarmot.lua

--- utmarmot.lua
+++ utmarmot.lua
@@ -22,7 +22,7 @@ getgenv().pvbConfig = {
     KEEP_PLANT_RARITY = {"Secret", "Limited"},
     KEEP_BRAINROT_MONEY_PER_SECOND = 500000,
     KEEP_BRAINROT_RARITY = {},
-    KEEP_BRAINROT = {"Crazylone Pizaione","Los Mr Carrotitos","Los Sekolitos","La Tomatoro","Los Tralaleritos"},
+    KEEP_BRAINROT = {"Crazylone Pizaione", "Los Mr Carrotitos", "Los Sekolitos", "La Tomatoro", "Dragon Cannelloni", "Los Tralaleritos"},
 
     SELL_BRAINROT_DELAY = 30,
     SELL_PLANT_DELAY = 30,
