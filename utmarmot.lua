cda08b2450eef0fe33fea0560f286999ff1e78c6
utnho123456 <baolonggl28022021@gmail.com>
2025-11-08 22:06:38 +0700

    Update utmarmot.lua

--- utmarmot.lua
+++ utmarmot.lua
@@ -1,21 +1,21 @@
 script_key="KuWflMRzMZOxYfxiPEEBspiUMvtsloOX";
 
-getgenv().pvbConfig = {
+getgenv().pvbConfig = {  
     AUTO_UPDATE_RESTART = true,
     MAX_FPS = 2,  -- This will override setfpscap()
     LOW_CPU = true,
     MAX_REBIRTH = 99,  -- Stop rebirth at set amount
-    USE_XP_BOTTLE = true,  -- Use XP Bottle on highest damage plant
+    USE_XP_BOTTLE = false,  -- Use XP Bottle on highest damage plant
     FROST_GRENADE_TARGET_MAX_HP = 100000,  -- Use frost grenade 100k+ hp brainrot
     CONSUME_POTION_EVENT = {"Frozen", "Golden", "Rainbow", "Galactic", "Underworld", "UpsideDown", "Volcano"},
     PLANT_SECRET_LIMITED_SEED_EVENT = {"Frozen", "Golden", "Rainbow", "Galactic", "Underworld", "UpsideDown", "Volcano"},
-    BUY_EVENT_SHOP = {"Halloween Card Pack", "Candy Brainrot Crate", "Candy Spooky Crate"},
     
     OPEN_LUCKY_EGG = {"Godly Lucky Egg", "Secret Lucky Egg", "Meme Lucky Egg"},
     OPEN_CARD_PACK = {"Base", "Shiny Base", "Halloween"},
     MERGE_CARD_RARITY = {"Common", "Uncommon", "Rare", "Epic", "Legendary"},
     USE_PLANT_EXTRACTOR_RARITY = {"Common", "Uncommon", "Rare", "Epic", "Legendary"},
-    FUSE_PLANT = {"Watermelon", "Eggplant", "Dragon Fruit", "Sunflower", "Pumpkin"},  -- Auto keep (favorite) & fuse required plant + brainrot
+    FUSE_PLANT = {"Mr Carrot", "Watermelon", "Eggplant", "Dragon Fruit", "Sunflower", "Pumpkin"},  -- Auto keep (favorite) & fuse required plant + brainrot
+    MERGE_MUTATION_NAME = {},
 
     BUY_SEED_SHOP = {["Cactus"] = 5, ["Aubie"] = 5, ["Tomade Torelli"] = 5, ["Grape"] = 5, ["Copuccino"] = 5, ["Sunzio"] = 5,  ["Strawberry"] = 5, ["Pumpkin"] = 5, ["Sunflower"] = 5, ["Dragon Fruit"] = 5, ["Eggplant"] = 5, ["Watermelon"] = 5, ["Cocotank"] = 5, ["Carnivorous Plant"] = 5, "Mr Carrot", "Tomatrio", "Shroombino", "Mango", "King Limone", "Starfruit"},
     BUY_GEAR_SHOP = {"Frost Grenade", "Frost Blower"},
