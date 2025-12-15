e7a6134bfb8354806f4763832a983d43fecddeaf
utnho123456 <baolonggl28022021@gmail.com>
2025-12-15 17:30:11 +0700

    Update utmarmot.lua

--- utmarmot.lua
+++ utmarmot.lua
@@ -1,127 +1,29 @@
-_G.FishItConfig = _G.FishItConfig or {
-    ["Fishing"] = {
-        ["Auto Perfect"] = true,
-        ["Auto Favorite"] = false,
-        ["Fish Name"] = {
-            "Gar Fish",
-        },
-        ["Auto Accept Trade"] = true,
-    },
-    ["Auto Trade"] = {
-        ["Enabled"] = false,
-        ["Whitelist Username"] = {""},
-        ["Category Fish"] = { -- Only Select one, fill in the fish category or fish name
-            "Secret",
-        },
-        ["Fish Name"] = {
-        },
-    },
-    ["Farm Coin Only"] = {
-        ["Enabled"] = false, -- Farm coins only [ cant buy rod, bait, enchant, weather ]
-        ["Target"] = 190000,
-    },
-    ["Selling"] = {
-        ["Auto Sell"] = true,
-        ["Auto Sell Threshold"] = "Legendary",
-        ["Auto Sell Every"] = 100,
-    },
-    ["Doing Quest"] = {
-        ["Auto Ghostfinn Rod"] = true,
-        ["Auto Element Rod"] = false,
-        ["Allowed Sacrifice"] = {
-            "Blob Shark",
-            "Ghost Shark",
-        },
-        ["FARM_LOC_SECRET_SACRIFICE"] = "Ocean",
-
-        ["Minimum Rod"] = "Astral Rod",
-    },
-    ["WebHook"] = {
-        ["Link Webhook"] = "",
-        ["Auto Sending"] = false,
-        ["Category"] = {"Secret"},
-
-        ["Link Webhook Quest Complete"] = "",
-    },
-    ["Weather"] = {
-        ["Auto Buying"] = true,
-        ["Minimum Rod"] = "Astral Rod",
-        ["Weather List"] = {
-            "Shark Hunrk",
-            "Storm", 
-            "Wind",
-        },
-    },
-    ["Potions"] = {
-        ["Auto Use"] = true,
-        ["Minimum Rod"] = "Astral Rod",
-    },
-    ["Event"] = {
-        ["Start Farm"] = true,
-        ["Minimum Rod"] = "Ghostfinn Rod",
-        ["Event List"] = {
-            "Megalodon Hunt",
-            "Ghost Shark Hunt",
-            "Shark Hunt",
-        },
-    },
-    ["Enchant"] = {
-        ["Auto Enchant"] = true,
-        ["Roll Enchant"] = true,
-        ["Enchant List"] = {
-            "Leprechaun II",
-            "Mutation Hunter II",
-            "Prismatic I",
-        },
-        ["Second Enchant"] = false,
-        ["Allowed Sacrifice"] = {
-            "Blob Shark",
-            "Cryoshade Glider",
-        },
-        ["Second Enchant List"] = {
-            "Reeler I",
-            "Prismatic I",
-            "Mutation Hunter II",
-        },
-        ["Minimum Rod"] = "Ghostfinn Rod",
-    },
-    ["Bait List"] = {
-        ["Auto Buying"] = true,
-        ["Buy List"] = {
-            "Midnight Bait",
-            "Chroma Bait",
-            "Corrupt Bait",
-            "Aether Bait",
-	    "Singularity Bait",
-        },
-    },
-    ["Rod List"] = {
-        ["Auto Buying"] = true,
-        ["Buy List"] = {
-            "Grass Rod",
-            "Midnight Rod",
-            "Astral Rod",
-            "Ares Rod",
-            "Angler Rod",
-            "Ghostfinn Rod",
-        },
-        ["Location Rods"] = { -- Farm location and minimum rod to start on that location, also only 1 rod and only 1 location
-            ["Fisherman Island"] = {"Starter Rod"},
-            ["Kohana Volcano"] = {"Grass Rod", "Midnight Rod"},
-            ["Tropical Grove"] = {"Astral Rod"},
-            ["Esoteric Depths"] = {"Element Rod", "Ghostfinn Rod", "Angler Rod", "Ares Rod"},
-        },
-    },
-
-    ["ExtremeFpsBoost"] = true,
-    ["UltimatePerformance"] = false,
-    ["Disable3DRender"] = true,
-    ["AutoRemovePlayer"] = true,
-
-    ["AutoReconnect"] = true,
-    ["HideGUI"] = false,
-    ["EXIT_MAP_IF_DISCONNECT"] = true,
+script_key = "VhGFIwqBLbitiaIrqeWUJybfRcXlsFUr"
+getgenv().Config = {
+    FARM_MODE = "BattlePass", -- BattlePass / Crate
+    AUTO_RESTART_WHEN_UPDATE = true,
+    AUTO_SHUTDOWN_WHEN_HOP_ERROR = false, -- If error please use, please use and check before going offline
+    DISPLAY_UI = true,
+    REJOIN_WHEN_STUCK_LOADING_AFTER = 300, -- Customize if u find it too fast, depending on your network
+    STOP_AUTO_BUY_FINAL_REWARD = false, -- Stop auto buy Raygun in FARM_MODE = "BattlePass"
+    WEBHOOK_URL = "https://discord.com/api/webhooks/1427232619929669693/eMs_h63iFj7aOadVNq1uDK7-OJH6SKwhU0pdfRj3G3qHDEinlZSkmWnVHUpYXqNF0UCq",
+    WEBHOOK_NOTE = "Yuri"
 }
-script_key="E934DA43917BBF9FF3B0877EF1D68599";
 
-local s,r repeat s,r=pcall(function()return game:HttpGet("https://api.wintercode.dev/loader/fishit-78c86024ea87c8eca577549807421962.lua")end)wait(1)until s;loadstring(r)()
+-- No edits
+task.spawn(function()
+    local LastCheck = tick()
+    while task.wait() do
+        if getgenv().ScriptLoaded then
+            break
+        end
+
+        pcall(function()
+            if LastCheck + 180 <= tick() then
+                game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
+            end
+        end)
+    end
+end)
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8e7c89050d8f703ed98b1f594b515d22.lua"))()
