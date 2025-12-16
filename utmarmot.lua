dbb793a88760344ce8b9e204e8397617082e30b1
utnho123456 <baolonggl28022021@gmail.com>
2025-12-16 13:57:28 +0700

    Update utmarmot.lua

--- utmarmot.lua
+++ utmarmot.lua
@@ -2,15 +2,12 @@ script_key = "VhGFIwqBLbitiaIrqeWUJybfRcXlsFUr"
 getgenv().Config = {
     FARM_MODE = "BattlePass", -- BattlePass / Crate
     AUTO_RESTART_WHEN_UPDATE = true,
-    AUTO_SHUTDOWN_WHEN_HOP_ERROR = false, -- If error please use, please use and check before going offline
     DISPLAY_UI = true,
-    REJOIN_WHEN_STUCK_LOADING_AFTER = 300, -- Customize if u find it too fast, depending on your network
-    STOP_AUTO_BUY_FINAL_REWARD = false, -- Stop auto buy Raygun in FARM_MODE = "BattlePass"
+    STOP_AUTO_BUY_FINAL_REWARD = false, -- Stop auto buy Godly Item when completed Battle Pass
     WEBHOOK_URL = "https://discord.com/api/webhooks/1427232619929669693/eMs_h63iFj7aOadVNq1uDK7-OJH6SKwhU0pdfRj3G3qHDEinlZSkmWnVHUpYXqNF0UCq",
-    WEBHOOK_NOTE = "Yuri"
+    WEBHOOK_NOTE = ""
 }
 
--- No edits
 task.spawn(function()
     local LastCheck = tick()
     while task.wait() do
@@ -26,4 +23,4 @@ task.spawn(function()
     end
 end)
 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/493f6c6b0b036f044d46ffb43292b15e.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8b381051c5e99846fe64dcea7d597e49.lua"))()
