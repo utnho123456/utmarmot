e027ab8730438636e3d312eb651f9631914517c2
utnho123456 <baolonggl28022021@gmail.com>
2026-03-18 15:37:43 +0700

    Update utmarmot.lua

--- utmarmot.lua
+++ utmarmot.lua
@@ -1,4 +1,4 @@
-script_key = "KwNYrCvahAUFKwgJnMcdcJwZIYnVlpLw"
+script_key = "TNZgQpciUnVNHbPloyLtrnSqMippGElP"
 getgenv().Config = {
     LOCK_FPS = 3, -- Only work in Web Version
     FARM_MODE = "Crate", -- Crate Only
