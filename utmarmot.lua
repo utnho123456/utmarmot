24f5bcdfe9416809ac7975d727387166840ba5ce
utnho123456 <baolonggl28022021@gmail.com>
2026-05-15 09:47:39 +0700

    Update utmarmot.lua

--- utmarmot.lua
+++ utmarmot.lua
@@ -1,6 +1,6 @@
 script_key = "XTUhpiQBCymyHQHilFaMuEbxHGxDPcPV"
 getgenv().Config = {
-    TargetStand = {"Tusk","TuskAlter","STW","DiosTheWorld","TheWorldGreatestHigh","StarFlatinum","OVATheWorld","ChefCrimson","SoftMachine","JellybeanTheWorld"},
+    TargetStand = {"Tusk","TuskAlter","STW","TheWorld","DiosTheWorld","TheWorldGreatestHigh","StarFlatinum","OVATheWorld","ChefCrimson","SoftMachine","JellybeanTheWorld"},
     AutoChange = { -- Only one can be set to true.
         Yummy = {
             Enabled = true,
@@ -9,7 +9,8 @@ getgenv().Config = {
         FarmSync = {
             Enabled = false,
             OriginalID = "",
-            CompletedID = ""
+            CompletedID = "",
+            ConfigId = ""
         },
         MouseFarm = {
             Enabled = false,
@@ -17,8 +18,9 @@ getgenv().Config = {
     },
     Webhook = {
         Url = "https://discord.com/api/webhooks/1504523117379452948/FW8ewfdvCIG6CB9ZX4gZqAwGrNnWGHYWWDJ4JR4Zq-tz5kkwv4PNpezjIqqbEVndTV2Q",
-        Ping = "<@everyone>" -- @everyone or Discord ID
+        Ping = "<@>" -- @everyone or Discord ID
     },
     Performance = {FPSLock = 10,LowCPU = true}
 }
+task.delay(180, function() if not getgenv().Loaded then game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId,game.JobId) end end)
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/dc8d11deda163dc93df08a85ba583739.lua"))()
