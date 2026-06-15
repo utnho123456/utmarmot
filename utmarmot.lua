70d7673873c6ba88c64cb9dbfcb5bb1034df30b1
utnho123456 <baolonggl28022021@gmail.com>
2026-06-15 21:03:30 +0700

    Update utmarmot.lua

--- utmarmot.lua
+++ utmarmot.lua
@@ -1,46 +1,107 @@
-script_key="vGoBSjwfYJQxHwrgaQPIXeaUJVgDaqqC";
-getgenv().AOTR = {
-    Raid = {
-        Enabled     = false,
-        Map         = "Chapel",
-        Objective   = "Attack Titan",
-        Difficulty  = "Hard",
-        LevelToRaid = 275,
-    },
-    Modifiers = {
-        ["No Perks"]         = true,
-        ["No Skills"]        = true,
-        ["No Memories"]      = true,
-        ["Nightmare"]        = true,
-        ["Oddball"]          = true,
-        ["Injury Prone"]     = true,
-        ["Chronic Injuries"] = true,
-        ["Fog"]              = true,
-        ["Time Trial"]       = true,
-        ["Glass Cannon"]     = true,
-        ["Boring"]           = false,
-        ["Simple"]           = false,
+script_key="EdJffLJoUyBDcKCPrCUwXcotPITxanBM";
+getgenv().GAG2Config = {
+    FPS_CAP = 5,
+    AUTO_UPDATE_RESTART = true,
+
+    BUY_PET = {  -- ["Pet Name"] = Amount
+        ["Monkey"] = 99, 
+        ["Bee"] = 99, 
+        ["BlackDragon"] = 99, 
+        ["GoldenDragonfly"] = 99, 
+        ["Unicorn"] = 99, 
+        ["Raccoon"] = 99, 
+        ["IceSerpent"] = 99,
+
+        ["Robin"] = 5,
+        ["Deer"] = 5,
     },
 
-    Boosts = {
-        AutoRebuy   = true,
-        MinCanesKeep = 0,  
-        Items = {
-            XP   = { ["30m"] = true, ["1h"] = false, ["2h"] = false },
-            Gold = { ["30m"] = true, ["1h"] = false, ["2h"] = false },
-            Luck = { ["30m"] = true, ["1h"] = false, ["2h"] = false },
-        },
+    EQUIP_PET = {  -- {"Pet Name", Amount to Equip, Priority}
+        {"Unicorn", 5, 1}, 
+        {"GoldenDragonfly", 10, 2},
+        {"Robin", 5, 3},
+        {"Deer", 5, 4},
     },
 
-    AutoPrestige = {
-        Enabled = true,
-        GoldRequired = {
-            [0] = 200000000,   
-            [1] = 400000000,    
-            [2] = 600000000,   
-            [3] = 800000000,    
-            [4] = 1000000000,   
-        },
+    PLANT_SEED = {  -- ["Seed Name"] = Amount
+        ["Carrot"] = 50, 
+        ["Strawberry"] = 4,
+        ["Blueberry"] = 4,
+        ["Tulip"] = 50,
+        ["Tomato"] = 4,
+        ["Apple"] = 4,
+        ["Bamboo"] = 50,
+        ["Corn"] = 4,
+        ["Cactus"] = 4,
+        ["Pineapple"] = 4,
+        ["Mushroom"] = 50,
+        ["Green Bean"] = 50,
+        ["Banana"] = 50,
+        ["Grape"] = 50,
+        ["Coconut"] = 50,
+        ["Mango"] = 50,
+        ["Dragon Fruit"] = 50,
+        ["Acorn"] = 50,
+        ["Cherry"] = 50,
+        ["Sunflower"] = 50,
+        ["Venus Fly Trap"] = 50,
+        ["Pomegranate"] = 50,
+        ["Poison Apple"] = 50,
+        ["Moon Bloom"] = 50,
+        ["Dragon's Breath"] = 50,
     },
+
+    BUY_SEED = {  -- ["Seed Name"] = Amount
+        ["Carrot"] = 999, 
+        ["Strawberry"] = 999,
+        ["Blueberry"] = 999,
+        ["Tulip"] = 999,
+        ["Tomato"] = 999,
+        ["Apple"] = 999,
+        ["Bamboo"] = 999,
+        ["Corn"] = 999,
+        ["Cactus"] = 999,
+        ["Pineapple"] = 999,
+        ["Mushroom"] = 999,
+        ["Green Bean"] = 999,
+        ["Banana"] = 999,
+        ["Grape"] = 999,
+        ["Coconut"] = 999,
+        ["Mango"] = 999,
+        ["Dragon Fruit"] = 999,
+        ["Acorn"] = 999,
+        ["Cherry"] = 999,
+        ["Sunflower"] = 999,
+        ["Venus Fly Trap"] = 999,
+        ["Pomegranate"] = 999,
+        ["Poison Apple"] = 999,
+        ["Moon Bloom"] = 999,
+        ["Dragon's Breath"] = 999,
+    },
+
+    COLLECT_PLANT_IF_MUTATED = { "Bamboo", "Mushroom", "Green Bean" },  -- Only collect fruit when have ANY mutation
+    FAVOURITE_FRUIT = {},  -- ["Fruit Name"] = {} ... or ... ["Fruit Name"] = { "Gold", "Rainbow" }
+
+    SELL_ALL_DELAY = 20,
+    EXPAND_PLOT = false,
+    BUY_GEAR = {},  -- ["Gear Name"] = Amount
+    BUY_CRATE = {},  -- ["Crate Name"] = Amount
+    USE_SPRINKLER = {},  -- "Sprinkler Name"
+
+    -- Auto Mail
+    AUTO_MAIL_USERNAME = {"Yurile2501"},  -- "Username" (Randomized)
+    AUTO_MAIL_ITEM_NAME = {["Rainbow"] = 1, ["Gold"] = 1, ["Raccoon"] = 1, ["Unicorn"] = 1},  -- ["Item Name"] = Amount
+    COLLECT_MAIL = true,
+
+    -- Discord
+    WEBHOOK_PET_NAME = {},
+    WEBHOOK_PET_RARITY = { "Mythic", "Super", "Secret" },
+    WEBHOOK_URL = "https://discord.com/api/webhooks/1516080485808210104/nVX9SFhS_OHBYIxsQL5HYd5FhRS-YjcwG6kCleOfOHCbR843MoWYVIa_7fCSknSIyU0X",
+    DISCORD_ID = "1344518159587348482",
+    WEBHOOK_NOTE = "",  -- Private Webhook
+    SHOW_PUBLIC_DISCORD_ID = true,
+    SHOW_WEBHOOK_USERNAME = true,  -- Private Webhook
+    SHOW_WEBHOOK_JOBID = true,  -- Private Webhook
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d6882ca362327ff6d4b1837fe0e4c809.lua"))()
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/c9ea3aa6c782de82dee4bcf6b9a9d03e.lua"))()
